40a82b152c17cc576782a5ca653cb8cf337f2cc0
Vojtech Vitek (V-Teq) <vvitek@redhat.com>
2014-08-16 02:03:13 +0200

    Add BoardMembers() client method

---
+++ board.go
@@ -0,0 +1,52 @@
+/*
+Copyright 2014 go-trello authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package trello
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Board struct {
+	Id string `json:"id"`
+}
+
+func (c *Client) BoardMembers(boardId string) (members []Member, err error) {
+	req, err := http.NewRequest("GET", c.endpoint+"/boards/"+boardId+"/members", nil)
+	if err != nil {
+		return
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	} else if resp.StatusCode != 200 {
+		err = fmt.Errorf("Received unexpected status %d while trying to retrieve the server data", resp.StatusCode)
+		return
+	}
+
+	err = json.Unmarshal(body, &members)
+	return
+}
