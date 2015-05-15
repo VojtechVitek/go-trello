0f2afb47504816677e7d21ae2a870b67c9dfe8d1
jeromenerf <jerome.andrieux@af83.com>
2015-05-15 17:55:55 +0200

    attributes type changes

--- attachment.go
+++ attachment.go
@@ -21,11 +21,18 @@ type Attachment struct {
 	Id        string   `json:"id"`
 	Bytes     int      `json:"bytes"`
 	Date      string   `json:"date"`
-	EdgeColor []string `json:"edgeColor"`
+	EdgeColor string   `json:"edgeColor"`
 	IdMember  string   `json:"idMember"`
 	IsUpload  bool     `json:"idUpload"`
 	MimeType  []string `json:"mimeType"`
 	Name      string   `json:"name"`
-	Previews  []string `json:"previews"`
-	Url       string   `json:"url"`
+	Previews  []struct {
+		Width  int    `json:"width"`
+		Height int    `json:"height"`
+		Url    string `json:"url"` // gophers usually prefer URL...
+		Bytes  int    `json:"bytes"`
+		Id     string `json:"_id"`
+		Scaled bool   `json:"scaled"`
+	} `json:"previews"`
+	Url string `json:"url"`
 }
