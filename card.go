2d205bca0f9ee0e5235d55388cec3e11fea8243f
Knut Ahlers <knut@ahlers.me>
2016-02-06 21:54:36 +0100

    Allow moving a card

--- card.go
+++ card.go
@@ -188,3 +188,12 @@ func (c *Card) Archive() ([]byte, error) {
 
 	return c.client.Put("/cards/"+c.Id+"/closed", payload)
 }
+
+// MoveToList will move the card to another list
+// https://developers.trello.com/advanced-reference/card#put-1-cards-card-id-or-shortlink-idlist
+func (c *Card) MoveToList(list *List) ([]byte, error) {
+	payload := url.Values{}
+	payload.Set("value", list.Id)
+
+	return c.client.Put("/cards/"+c.Id+"/idList", payload)
+}
