a9eb51faf8df312b4b7aebf33c07efb6cf63239b
Joonas Myhrberg <joonas.myhrberg@gmail.com>
2016-10-11 17:29:13 +0300

    Add method to list webhooks

--- webhook.go
+++ webhook.go
@@ -1,9 +1,29 @@
 package trello
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type Webhook struct {
 	ID          string `json:"id"`
 	Description string `json:"description"`
 	IDModel     string `json:"idModel"`
 	CallbackURL string `json:"callbackURL"`
 	Active      bool   `json:"active"`
+}
+
+func (c *Client) Webhooks(token string) (webhooks []Webhook, err error) {
+
+	body, err := c.Get(webhookURL(token))
+	if err != nil {
+		return []Webhook{}, err
+	}
+	err = json.Unmarshal(body, &webhooks)
+	return
+}
+
+func webhookURL(token string) (url string) {
+
+	return fmt.Sprintf("/tokens/%s/webhooks/", token)
 }
