3712746653144bc1cad1a24c36979577aa60483e
Joonas Myhrberg <joonas.myhrberg@gmail.com>
2016-10-12 15:47:29 +0300

    Add method to get webhook info

--- webhook.go
+++ webhook.go
@@ -23,6 +23,17 @@ func (c *Client) Webhooks(token string) (webhooks []Webhook, err error) {
 	return
 }
 
+func (c *Client) Webhook(webhookID string) (webhook Webhook, err error) {
+
+	url := fmt.Sprintf("/webhooks/%s/", webhookID)
+	body, err := c.Get(url)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &webhook)
+	return
+}
+
 func webhookURL(token string) (url string) {
 
 	return fmt.Sprintf("/tokens/%s/webhooks/", token)
